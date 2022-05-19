7e9e48da8f4eec0b4e2d58cba2c2e41e99bdb6c7
skxlpv <olexandertym@gmail.com>
2022-05-19 14:57:09 +0300

    Moved .env into venv dir. Updated requirements.txt with a cors headers.

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ environs.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environs.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environs.Env.read_env(os.path.join(BASE_DIR, 'venv/.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
