1e3111cc1bf9b4257cb45e16f0a5a14cd4a781be
skxlpv <olexandertym@gmail.com>
2022-05-26 15:43:34 +0300

    Hospital creation. Create a city with a name and a region. Create a hospital with a name and link a city to it. Create a department with a name and link a hospital to it.

--- hospitals/admin.py
+++ hospitals/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Cities, Hospitals
+from .models import Cities, Hospitals, Departments
 
 admin.site.register(Cities)
 admin.site.register(Hospitals)
+admin.site.register(Departments)
--- hospitals/models.py
+++ hospitals/models.py
@@ -10,32 +10,29 @@ class Cities(models.Model):
         return f"{self.city}, {self.region}"
 
     class Meta:
-        ordering = ('-region', )
         verbose_name = _('City')
         verbose_name_plural = _('Cities')
 
 
 class Hospitals(models.Model):
     hospital_name = models.CharField(max_length=255)
-    region = models.ForeignKey(Cities, on_delete=models.CASCADE)
+    hospital_city = models.ForeignKey(Cities, on_delete=models.CASCADE, related_name='cities', default='')
 
     def __str__(self):
-        return f"{self.hospital_name},  {self.region.city}"
+        return f"{self.hospital_name}"
 
     class Meta:
-        ordering = ('-region', )
         verbose_name = _('Hospital')
         verbose_name_plural = _('Hospitals')
 
 
 class Departments(models.Model):
-    name = models.CharField(max_length=255)
-    hospital_name = models.ForeignKey(Hospitals, on_delete=models.CASCADE)
+    department_name = models.CharField(max_length=255)
+    hospital_name = models.ForeignKey(Hospitals, on_delete=models.CASCADE, related_name='hospital')
 
     def __str__(self):
-        return f"{self.name}, {self.hospital_name}"
+        return f"{self.department_name}"
 
     class Meta:
-        ordering = ('-hospital_name',)
         verbose_name = _('Departament')
         verbose_name_plural = _('Departments')
