8040220cc7bee70bcb7de99dc708550b8922990d
skxlpv <olexandertym@gmail.com>
2022-05-29 18:18:43 +0300

    Added ability to patch item's quantity via put.

--- management/serializers.py
+++ management/serializers.py
@@ -8,3 +8,8 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        instance.quantity = validated_data['quantity']
+        instance.save()
+        return instance
--- management/views.py
+++ management/views.py
@@ -1,6 +1,6 @@
 import json
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -8,9 +8,11 @@ from management.models import Item
 from management.serializers import ItemSerializer
 
 
-class ItemViewSet(viewsets.ViewSet):
+class ItemViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, ]
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-    def list(self, request):
-        queryset = Item.objects.all()
-        serializer = ItemSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def partial_update(self, request, *args, **kwargs):
+        item_ser = self.get_serializer(Item, data=request.data, partial=True)
+        item_ser.save()
