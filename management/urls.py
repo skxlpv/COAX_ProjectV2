be29072e798270e96d440ca719604c3fe1237328
skxlpv <olexandertym@gmail.com>
2022-05-31 13:38:29 +0300

    Rearranged the urls in management.urls. /v1/categories/ - show all the categories /v1/categories/<category-name> - search categories by url /v1/categories/items/ - show all the items /v1/categories/items/<some-item> - search items by url

--- management/urls.py
+++ management/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from management.views import ItemViewSet, CategoryViewSet
 
 router = DefaultRouter()
-router.register(r'items', ItemViewSet, basename='items')
+router.register(r'categories/items', ItemViewSet, basename='items')
 router.register(r'categories', CategoryViewSet, basename='categories')
 
 urlpatterns = router.urls
