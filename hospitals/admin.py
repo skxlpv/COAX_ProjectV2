29b773755b5c0acf8afef6c077b348382d06a89b
skxlpv <olexandertym@gmail.com>
2022-06-06 09:27:41 +0300

    Filter horizontal departments

--- hospitals/admin.py
+++ hospitals/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import City, Hospital, Department
 
 
-# class Admin(admin.ModelAdmin):
-#     filter_horizontal = ['hospital_departments']
+class Admin(admin.ModelAdmin):
+    filter_horizontal = ['hospital_departments']
 
 
 admin.site.register(City)
-admin.site.register(Hospital)
+admin.site.register(Hospital, Admin)
 admin.site.register(Department)
