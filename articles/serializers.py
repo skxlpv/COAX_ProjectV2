a6206f6b0cfcca99a06fd7e160756e2906c225d2
palibrix <theopaolo04@gmail.com>
2022-06-06 15:43:42 +0300

    Хакуна Матата

--- articles/serializers.py
+++ articles/serializers.py
@@ -13,42 +13,40 @@ Field.default_error_messages = {
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField()
-
     class Meta:
         model = Categories
         fields = ('id', 'name')
-        read_only_fields = ('name',)
+        read_only_fields = ('id', 'name',)
 
 
 class EditArticleSerializer(serializers.ModelSerializer):
-    category = CategoriesSerializer()
+    category = CategoriesSerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Categories.objects.all().values_list('id', flat=True),
+        write_only=True
+    )
 
     class Meta:
         model = Articles
-        fields = ('id', 'title', 'excerpt', 'text', 'category', 'status')
+        fields = ('id', 'title', 'excerpt', 'text', 'category', 'category_id', 'status')
         read_only_fields = ('id',)
 
 
 class ArticlesSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     hospital = HospitalSerializer(read_only=True)
-    category = CategoriesSerializer()
+    category = CategoriesSerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Categories.objects.all().values_list('id', flat=True),
+        write_only=True
+    )
 
     class Meta:
         model = Articles
-        fields = ('id', 'status', 'title', 'excerpt', 'text', 'category', 'author', 'hospital')
+        fields = ('id', 'status', 'title', 'excerpt', 'text', 'category', 'category_id', 'author', 'hospital')
         read_only_fields = ('id',)
-
-    def validate_category(self, value):
-        try:
-            Categories.objects.get(id=value["id"])
-        except Exception as e:
-            raise serializers.ValidationError('No such category')
-        return value
 
     @transaction.atomic
     def create(self, validated_data):
-        category = validated_data.pop('category')
-        validated_data['category_id'] = category['id']
+        validated_data['category_id'] = validated_data.pop('category_id')
         return super(ArticlesSerializer, self).create(validated_data)
--- articles/views.py
+++ articles/views.py
@@ -1,21 +1,11 @@
 from rest_framework import mixins, serializers, viewsets, status
 from rest_framework.decorators import api_view, permission_classes, action
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
-
+from articles import serializers as ser
 from api.permissions import IsWriter, IsLeader, IsHelper, IsCommon, HasArticleUpdate  # all the available permissions
 from articles.models import Articles, Categories
 from articles.serializers import ArticlesSerializer, EditArticleSerializer
-from users.models import User
-
-
-# class AddArticleViewSet(mixins.CreateModelMixin,
-#                         GenericViewSet):
-#     permission_classes = (IsAuthenticated & IsWriter)
-#     serializer_class = ArticlesSerializer
-#
-#
 
 
 class ArticlesViewSet(mixins.ListModelMixin,
@@ -24,18 +14,52 @@ class ArticlesViewSet(mixins.ListModelMixin,
                       mixins.UpdateModelMixin,
                       mixins.CreateModelMixin,
                       GenericViewSet):
+    """
+    list:
+    Get list of articles
 
-    serializer_class = ArticlesSerializer
+    ## Get detailed information about articles
+
+    URLs
+    ```python
+    All articles:
+    GET http://127.0.0.1:8000/v1/articles/
+
+    Specific article:
+    GET http://127.0.0.1:8000/v1/articles/x
+    ```
+
+    create:
+    Create article
+
+    URL
+    ```python
+    POST http://127.0.0.1:8000/v1/articles/
+    ```
+
+    Request example
+    ```python
+    {
+      "title": "string",
+      "excerpt": "string",
+      "text": "string",
+      "category_id": int
+    }
+    ```
+
+    """
+
     permission_classes = (IsAuthenticated, HasArticleUpdate, IsWriter)
 
+    def get_serializer_class(self):
+        if self.action in ('update', 'partial_update',):
+            return ser.EditArticleSerializer
+        return ser.ArticlesSerializer
+
     def get_queryset(self):
-        return Articles.objects.all()
-        # return Articles.objects.filter(hospital=self.request.user.hospital)
+        # return Articles.objects.all()
+        return Articles.objects.filter(hospital=self.request.user.hospital)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, hospital=self.request.user.hospital)
-
-    def perform_update(self, serializer):
-        category = Categories.objects.get(id=self.request.data['category']['id'])
-        serializer.save(category=category)
 
