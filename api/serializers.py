782210b051307be15d77fd119f118d47d01c80f7
palibrix <theopaolo04@gmail.com>
2022-06-08 11:53:12 +0300

    Changed serializers and rebased renaming

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-# from rest_framework import serializers
-# from articles.models import Article
-# from users.models import User
-#
-#
-# class ArticleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Article
-#         fields = ('id', 'category', 'title', 'excerpt', 'text', 'author', 'hospital', 'status')
