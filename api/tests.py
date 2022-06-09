dc64156f68fe5911324c3f173372f2acc7d3caf7
skxlpv <olexandertym@gmail.com>
2022-06-09 18:25:58 +0300

    Improved look of the code

--- api/tests.py
+++ api/tests.py
@@ -6,25 +6,34 @@ from rest_framework.test import APITestCase
 from rest_framework_simplejwt.settings import api_settings
 from rest_framework_simplejwt.tokens import AccessToken
 
+from hospitals.models import Hospital
 from users.models import User
 
 
 class BaseAPITest(APITestCase):
 
-    def create_super_user(self, email='test@test.com', password='1234512345',
-                          first_name='test', last_name='test'):
+    def create_user(self, email='test@test.com', password='1234512345',
+                    first_name='test', last_name='test',
+                    hospital=Hospital.objects.get(hospital_name='Hospital'),
+                    role='CM', **other_fields):
         user = User.objects.create_superuser(email=email, password=password,
-                                             first_name=first_name, last_name=last_name)
+                                             first_name=first_name, last_name=last_name,
+                                             hospital=hospital, role=role,
+                                             **other_fields)
         user.last_login = timezone.now()
         user.is_active = True
         user.save()
 
         return user
 
-    def create_and_login(self, email='test@test.com', password='1234512345',
-                         first_name='test', last_name='test'):
-        user = self.create_super_user(email=email, password=password,
-                                      first_name=first_name, last_name=last_name)
+    def create_user_and_login(self, email='test@test.com', password='1234512345',
+                              first_name='test', last_name='test',
+                              hospital=Hospital.objects.get(hospital_name='Hospital'),
+                              role='CM', **other_fields):
+        user = self.create_user(email=email, password=password,
+                                first_name=first_name, last_name=last_name,
+                                hospital=hospital, role=role,
+                                **other_fields)
         self.authorize(user)
         return user
 
--- management/tests.py
+++ management/tests.py
@@ -13,18 +13,19 @@ class TestItemViewSet(BaseAPITest):
 
     def setUp(self):
         self.department = mixer.blend(Department)
+        self.hospital = mixer.blend(Hospital)
         self.item = mixer.blend(Item)
 
-        self.update_data = {
+        self.user = self.create_user_and_login()
+
+        self.patch_data = {
             "id": 2,
             "category_name": 2,
-            "name": "MAniitor",
+            "name": "MAniitorRr",
             "description": "asopdiasopdi",
             "quantity": 15,
             "price_of_one": "40.00"
         }
-
-        self.user = self.create_and_login()
 
     # GET list of all the items
     def test_list_items_GET(self):
@@ -38,11 +39,11 @@ class TestItemViewSet(BaseAPITest):
 
     # PATCH item's quantity, no matter what other data was passed
     def test_update_item_PATCH(self):
-        resp = self.client.patch('/v1/management/items/1/', data=self.update_data)
+        resp = self.client.patch('/v1/management/items/1/', data=self.patch_data)
 
         self.assertEqual(resp.status_code, 200)
 
-        self.assertEqual(resp.data['quantity'], self.update_data['quantity'])
+        self.assertEqual(resp.data['quantity'], self.patch_data['quantity'])
 
         self.item.refresh_from_db()
-        self.assertEqual(self.item.quantity, self.update_data['quantity'])
+        self.assertEqual(self.item.quantity, self.patch_data['quantity'])
