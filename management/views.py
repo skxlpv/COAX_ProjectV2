e92d80dfe3facbec365a1ca4b02ca9a8f96768ae
skxlpv <olexandertym@gmail.com>
2022-06-11 16:12:19 +0300

    Added Small pagionation on Item list and Standart pagination on categories

--- management/views.py
+++ management/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from core.pagination import SmallResults, StandardResults
 from management.models import Item, Category
 from management.serializers import ItemSerializer, CategorySerializer
 
@@ -14,13 +15,12 @@ class ItemViewSet(mixins.ListModelMixin,
                   GenericViewSet):
     """
     Item
-
-    Item View Set
     """
 
     permission_classes = [IsAuthenticated, ]
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    pagination_class = SmallResults
 
     # def retrieve(self, request, *args, **kwargs):
     #     query_items = Item.objects.filter(name__istartswith=kwargs['pk'])
@@ -33,13 +33,12 @@ class CategoryViewSet(mixins.ListModelMixin,
                       GenericViewSet):
     """
     Category
-
-    Category View Set
     """
 
     permission_classes = [IsAuthenticated, ]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = StandardResults
 
     # def retrieve(self, request, *args, **kwargs):
     #     query_categories = Category.objects.filter(category_name__istartswith=kwargs['pk'])
