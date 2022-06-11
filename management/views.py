ed77a61d9f70e53cc2e5ecd3f84c8aaab3d24877
skxlpv <olexandertym@gmail.com>
2022-06-11 16:45:07 +0300

    DOCS management description

--- management/views.py
+++ management/views.py
@@ -1,7 +1,7 @@
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from core.pagination import SmallResults, StandardResults
 from management.models import Item, Category
@@ -12,9 +12,44 @@ class ItemViewSet(mixins.ListModelMixin,
                   mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin,
                   mixins.CreateModelMixin,
+                  mixins.DestroyModelMixin,
                   GenericViewSet):
     """
-    Item
+    list:
+
+    Item List
+
+    GET list of all the items
+
+    retrieve:
+
+    Item Detail
+
+    GET detailed view of a single item
+
+    destroy:
+
+    Item Destroy
+
+    DELETEs a single item
+
+    update:
+
+    Item Update
+
+    PUT item's quantity field only...¯|_ (ツ) _/¯
+
+    partial_update:
+
+    Item Patch
+
+    PATCH item's quantity field only
+
+    create:
+
+    Item Create
+
+    POST item
     """
 
     permission_classes = [IsAuthenticated, ]
@@ -32,7 +67,17 @@ class CategoryViewSet(mixins.ListModelMixin,
                       mixins.RetrieveModelMixin,
                       GenericViewSet):
     """
-    Category
+    list:
+
+    Category List
+
+    GET list of all the categories
+
+    retrieve:
+
+    Category Detail
+
+    GET detailed view of a single category
     """
 
     permission_classes = [IsAuthenticated, ]
