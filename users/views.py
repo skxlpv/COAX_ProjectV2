e22b915d95064d0db65a235c67d49bc29fa0c85a
skxlpv <olexandertym@gmail.com>
2022-06-16 15:55:48 +0300

    Base set up

--- users/views.py
+++ users/views.py
@@ -1,11 +1,33 @@
-from rest_framework import status
+from rest_framework import status, mixins
 from rest_framework.decorators import api_view
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import User
 
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  GenericViewSet):
+
+    permission_classes = (IsAuthenticated, )
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProfileViewSet(mixins.ListModelMixin,
+                     mixins.UpdateModelMixin,
+                     GenericViewSet):
+
+    permission_classes = (IsAuthenticated, )
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(email=self.request.user.email)
 
 class BlackListTokenView(APIView):
     permission_classes = [AllowAny]
@@ -20,11 +42,7 @@ class BlackListTokenView(APIView):
             return Response({'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# sends the current user data to the FrontEnd
 @api_view(['GET'])
 def current_user(request):
     user = request.user
-    return Response({
-        # user data, that should be passed to the FrontEnd
-        'email': user.email,
-    })
+    return Response({'email': user.email})
