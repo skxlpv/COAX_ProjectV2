d6c9f336fe9b72e9f0e39d31732b233d6fbd6138
skxlpv <olexandertym@gmail.com>
2022-06-26 12:47:45 +0300

    Events types

--- events/models.py
+++ events/models.py
@@ -4,10 +4,17 @@ from django.db import models
 
 
 class Event(models.Model):
+    TYPES = [
+        # may be expanded
+        (1, 'Seminar'),
+        (2, 'Conference'),
+    ]
+
     title = models.CharField(max_length=150, default='NO TITLE')
     description = models.CharField(max_length=250, default='NO DESCRIPTION')
-    start_time = models.DateTimeField(default=datetime.now)
-    end_time = models.DateTimeField(default=datetime.now)
+    type = models.PositiveIntegerField(choices=TYPES, null=False, blank=False, default=1)
+    start_time = models.DateTimeField(default=datetime.now())
+    end_time = models.DateTimeField(default=datetime.now())
 
     class Meta:
         verbose_name = 'Event'
