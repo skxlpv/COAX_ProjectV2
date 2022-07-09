75f3c147ab4cad196ce6b1b1c79b396051f19aae
skxlpv <olexandertym@gmail.com>
2022-07-09 18:30:36 +0300

    Event serializer update

--- events/serializers.py
+++ events/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
 from events.models import Event
+from hospitals.serializers import HospitalSerializer
+from users.serializers import AuthorSerializer
 
 
 class EventSerializer(serializers.ModelSerializer):
+    creator = AuthorSerializer(read_only=True)
+    hospital = HospitalSerializer(read_only=True)
     start_time = serializers.DateTimeField(format='%m/%d/%Y %H:%M', required=False, allow_null=True)
     end_time = serializers.DateTimeField(format='%m/%d/%Y %H:%M', required=False, allow_null=True)
 
     class Meta:
         model = Event
-        fields = ('id', 'title', 'description', 'type', 'start_time', 'end_time', 'participants')
+        fields = ('id', 'title', 'description', 'type',
+                  'start_time', 'end_time', 'participants',
+                  'creator', 'hospital')
         read_only_fields = ('start_time', 'end_time')
